db38c156384a67ac09ef284c362d1d07c7704ea2
Ameer Sabri <ameer.sabri@hotmail.co.uk>
2021-08-19 18:40:51 +0500

    made custom button

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+let billInput = document.getElementById("bill-input")
 let billAmount = 0;
+
 let tipPercentage = 0;
+
+let numPeopleInput = document.getElementById("bill-input")
 let numPeople = 0;
+
+let tipButtonSelected = false;
+let currentlySelectedButton = null;
 
 let totalAmountPerPerson = 0;
 let tipAmountPerPerson = 0;
 
-let finalTipAmountOutput = 0
-let finalTotalAmountOutput = 0
+let finalTipAmountOutput = 0;
+let finalTotalAmountOutput = 0;
+
+let resetBtn = document.getElementById("reset-btn")
 
 FivePercentBtn = document.getElementById("five-percent-btn");
 TenPercentBtn = document.getElementById("ten-percent-btn");
@@ -15,30 +24,46 @@ TwentyFivePercentBtn = document.getElementById("twenty-five-percent-btn");
 FiftyPercentBtn = document.getElementById("fifty-percent-btn");
 CustomPercentBtn = document.getElementById("custom-percent-input");
 
+
 FivePercentBtn.onclick = () => {
   tipPercentage = 0.05;
+  makeButtonStayPressed(FivePercentBtn);
   calculateTip();
 };
 TenPercentBtn.onclick = () => {
   tipPercentage = 0.1;
+  makeButtonStayPressed(TenPercentBtn);
   calculateTip();
 };
 FifteenPercentBtn.onclick = () => {
   tipPercentage = 0.15;
+  makeButtonStayPressed(FifteenPercentBtn);
   calculateTip();
 };
 TwentyFivePercentBtn.onclick = () => {
   tipPercentage = 0.25;
+  makeButtonStayPressed(TwentyFivePercentBtn);
   calculateTip();
 };
 FiftyPercentBtn.onclick = () => {
   tipPercentage = 0.5;
+  makeButtonStayPressed(FiftyPercentBtn);
   calculateTip();
 };
 CustomPercentBtn.onclick = () => {
+  makeButtonStayPressed(CustomPercentBtn);
   tipPercentage = Number(CustomPercentBtn.value) / 100;
   calculateTip();
 };
+
+resetBtn.onclick = () => {
+  console.log("reset")
+  document.getElementById("bill-input").value = ""
+  document.getElementById("num-people-input").value = ""
+  document.getElementById("calculated-tip-amount").textContent = "$0"
+  document.getElementById("calculated-total-amount").textContent = "$0"
+  currentlySelectedButton.classList.remove("tip-button-pressed");
+}
 
 function calculateTip() {
   console.log("Calculating Tip");
@@ -46,24 +71,24 @@ function calculateTip() {
   numPeople = Number(document.getElementById("num-people-input").value);
   finalTipAmountOutput = document.getElementById("calculated-tip-amount");
   finalTotalAmountOutput = document.getElementById("calculated-total-amount");
-  console.log(finalTipAmountOutput, finalTotalAmountOutput)
-  // if (FivePercentBtn.pressed) {
-  //   tipPercentage = 0.05;
-  // } else if (TenPercentBtn.pressed) {
-  //   tipPercentage = 0.10;
-  // } else if (FifteenPercentBtn.pressed) {
-  //   tipPercentage = 0.15;
-  // } else if (TwentyFivePercentBtn.pressed) {
-  //   tipPercentage = 0.25;
-  // } else if (FiftyPercentBtn.pressed) {
-  //   tipPercentage = 0.50;
-  // } else {
-  //   tipPercentage = Number(CustomPercentBtn.value) / 100;
-  // }
+  console.log(finalTipAmountOutput, finalTotalAmountOutput);
+
   console.log(tipPercentage, billAmount);
-  totalAmountPerPerson = Math.round(((1 + tipPercentage) * billAmount) / numPeople)
-  tipAmountPerPerson = Math.round((tipPercentage * billAmount) / numPeople)
-  finalTotalAmountOutput.textContent = "$" + totalAmountPerPerson;
-  finalTipAmountOutput.textContent = "$" + tipAmountPerPerson;
+  totalAmountPerPerson = (
+    ((1 + tipPercentage) * billAmount) / numPeople
+  );
+  tipAmountPerPerson = ((tipPercentage * billAmount) / numPeople);
+  finalTotalAmountOutput.textContent = "$" + totalAmountPerPerson.toFixed(2);
+  finalTipAmountOutput.textContent = "$" + tipAmountPerPerson.toFixed(2);
   console.log(totalAmountPerPerson);
+}
+
+function makeButtonStayPressed(button) {
+  if (tipButtonSelected) {
+    console.log("button already pressed", currentlySelectedButton);
+    currentlySelectedButton.classList.remove("tip-button-pressed");
+  }
+  tipButtonSelected = true;
+  currentlySelectedButton = button;
+  button.classList.add("tip-button-pressed");
 }
